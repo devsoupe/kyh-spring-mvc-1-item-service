cf64827912041edfe85917323c15a04355b762b6
devsoupe <luvyum4e@gmail.com>
2023-04-21 11:20:59 +0900

    프로젝트 생성

---
+++ src/main/java/hello/itemservice/KyhSpringMvc1ItemServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringMvc1ItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyhSpringMvc1ItemServiceApplication.class, args);
+    }
+
+}
